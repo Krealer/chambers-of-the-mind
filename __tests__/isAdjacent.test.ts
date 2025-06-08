4119b0fcf9805cfdea0d480856b3fc9e1ca61ae4
Krealer <Krealerkimizuki@gmail.com>
2025-06-08 23:31:05 +0100

    chore: add jest and basic isAdjacent test

---
+++ __tests__/isAdjacent.test.ts
@@ -0,0 +1,19 @@
+import { isAdjacent } from '../src/components/grid/Grid';
+
+describe('isAdjacent', () => {
+  test('returns true for horizontally adjacent tiles', () => {
+    expect(isAdjacent([0, 0], [0, 1])).toBe(true);
+  });
+
+  test('returns true for vertically adjacent tiles', () => {
+    expect(isAdjacent([2, 3], [3, 3])).toBe(true);
+  });
+
+  test('returns false for diagonal tiles', () => {
+    expect(isAdjacent([1, 1], [2, 2])).toBe(false);
+  });
+
+  test('returns false for distant tiles', () => {
+    expect(isAdjacent([0, 0], [0, 2])).toBe(false);
+  });
+});
